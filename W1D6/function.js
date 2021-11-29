0006947527ffe750ecce9fef8075ee77b321a922
sehailu2021 <94870026+sehailu2021@users.noreply.github.com>
2021-11-29 11:38:19 -0600

    functions

--- W1D6/function.js
+++ W1D6/function.js
@@ -25,12 +25,20 @@ const narray = [1,2,3,4,5,6]
  console.log(sum2(narray));
 
 
-function getNewArray(arr){
+ const getNewArray2 =function(arr2){
+    return arr2.filter(elem=>elem.length >=5 && elem.includes('a'))
+}
+const newArray = ["kalkidan", "serkalem","soli" ]
+console.log(getNewArray2(newArray));
+
+
+// or 
+ function getNewArray(arr){
     let ans = [];
     let j = 0;
     for(let i = 0; i<arr.length; i++){
-        if(arr[i].length >= 5  )
-        ans[j++] = arr[i];
+    if(arr[i].length >= 5  && arr[i].includes(a) )
+    ans[j++] = arr[i];
     }
     return ans;
 
@@ -38,9 +46,4 @@ function getNewArray(arr){
 const a = ["kalkidan", "serkalem","soli" ]
  console.log(getNewArray(a));
  
-function getNewArray2(arr2){
-    return arr2.filter(a=>a.length >=5)
-}
-const newArray = ["kalkidan", "serkalem","soli" ]
-console.log(getNewArray(newArray));
 
